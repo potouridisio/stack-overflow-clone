a4474382ee44d785e0140ec7fbda49ac33f2f513
Ioannis Potouridis <potouridisio@gmail.com>
2024-04-02 09:47:44 +0300

    Add questions, tags and users pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/main.ts'],
+  content: [
+    './index.html',
+    './src/main.ts',
+    './questions/index.html',
+    './questions/questions.ts',
+    './tags/index.html',
+    './tags/tags.ts',
+    './users/index.html',
+    './users/users.ts',
+  ],
   theme: {
     extend: {},
   },
