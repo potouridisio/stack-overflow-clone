0e9fcb64933b8efc31f7628068b7b7d55cc46862
Ioannis Potouridis <potouridisio@gmail.com>
2024-04-08 09:03:41 +0300

    Change pagination size

--- tags/tags.ts
+++ tags/tags.ts
@@ -36,17 +36,17 @@ function createPaginationElement(currentPage: number, totalPages: number) {
     <ul class="flex flex-wrap items-center">
       <li>
         <button
-          class="pointer-events-none mx-0.5 inline-flex h-8 min-w-8 cursor-default items-center justify-center rounded border border-black/20 px-1.5 text-sm text-black/90 opacity-40 hover:bg-black/5"
+          class="pointer-events-none mx-px inline-flex h-6 min-w-6 cursor-default items-center justify-center rounded border border-black/20 px-1 align-middle text-sm text-black/90 opacity-40 hover:bg-black/5"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
+            viewBox="0 0 16 16"
             fill="currentColor"
-            class="-mx-2 h-5 w-5"
+            class="-mx-2 h-4 w-4"
           >
             <path
               fill-rule="evenodd"
-              d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z"
+              d="M9.78 4.22a.75.75 0 0 1 0 1.06L7.06 8l2.72 2.72a.75.75 0 1 1-1.06 1.06L5.47 8.53a.75.75 0 0 1 0-1.06l3.25-3.25a.75.75 0 0 1 1.06 0Z"
               clip-rule="evenodd"
             />
           </svg>
@@ -54,17 +54,17 @@ function createPaginationElement(currentPage: number, totalPages: number) {
       </li>
       <li>
         <button
-          class="mx-0.5 inline-flex h-8 min-w-8 items-center justify-center rounded border border-black/20 px-1.5 text-sm text-black/90 hover:bg-black/5"
+          class="mx-px inline-flex h-6 min-w-6 items-center justify-center rounded border border-black/20 px-1 align-middle text-sm text-black/90 hover:bg-black/5"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
+            viewBox="0 0 16 16"
             fill="currentColor"
-            class="-mx-2 h-5 w-5"
+            class="-mx-2 h-4 w-4"
           >
             <path
               fill-rule="evenodd"
-              d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z"
+              d="M6.22 4.22a.75.75 0 0 1 1.06 0l3.25 3.25a.75.75 0 0 1 0 1.06l-3.25 3.25a.75.75 0 0 1-1.06-1.06L8.94 8 6.22 5.28a.75.75 0 0 1 0-1.06Z"
               clip-rule="evenodd"
             />
           </svg>
@@ -86,10 +86,8 @@ function createPaginationElement(currentPage: number, totalPages: number) {
 
     pageElement.innerHTML = `
       <button
-        class="mx-0.5 inline-flex h-8 min-w-8 items-center justify-center rounded border ${isCurrentPage ? 'border-rose-600/50' : 'border-black/20'}${isCurrentPage ? ' bg-rose-600/10' : ''} px-1.5 text-sm ${isCurrentPage ? 'text-rose-600 hover:bg-rose-600/25' : 'text-black/90 hover:bg-black/5'}"
-      >
-        ${page}
-      </button>
+        class="mx-px inline-flex h-6 min-w-6 items-center justify-center rounded border ${isCurrentPage ? 'border-rose-600/50' : 'border-black/20'}${isCurrentPage ? ' bg-rose-600/10' : ''} px-1 align-middle text-sm ${isCurrentPage ? 'text-rose-600 hover:bg-rose-600/25' : 'text-black/90 hover:bg-black/5'}"
+      >${page}</button>
     `
 
     nextButtonElement.insertAdjacentElement('beforebegin', pageElement)
